bd2fb572732c2ad8d70d66e320b3922318994b2f
Ivo Petrov <1414vo@gmail.com>
2024-06-12 14:17:16 +0000

    Lomb Scargle periodogram

---
+++ src/plot_utils.py
@@ -0,0 +1,42 @@
+from numpy.typing import NDArray
+import matplotlib.pyplot as plt
+from astropy.timeseries import LombScargle
+from matplotlib import rcParams
+
+
+def __modify_params():
+    """! Sets default matplotlib parameters."""
+    rcParams["pdf.fonttype"] = 42
+    rcParams["ps.fonttype"] = 42
+    rcParams["font.family"] = "serif"
+    rcParams["font.sans-serif"] = ["Palatio"]
+    rcParams["text.usetex"] = True
+    rcParams["text.latex.preamble"] = r"\usepackage{amsfonts}"
+
+
+def lomb_scargle(data: NDArray, out_path: str = None) -> float:
+    """! Plots the Lomb Scargle periodogram for the given data and
+    retrieves the most likely period.
+
+    @param data     The light curve time and flux.
+    @param out_path Where to store the plot.
+
+    @returns        The most likely period value."""
+    __modify_params()
+
+    plt.figure(figsize=(4, 4), dpi=150)
+    frequency, power = LombScargle(data[:, 0], data[:, 1]).autopower()
+    plt.plot(frequency, power, label="Frequency power")
+
+    plt.xlim(0, 1)
+    plt.xlabel("Frequency [days]")
+    plt.ylabel("Power")
+    plt.legend()
+    plt.tight_layout()
+
+    if out_path is None:
+        plt.show()
+    else:
+        plt.savefig(out_path)
+
+    return power[frequency.argmax()]
