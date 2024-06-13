0d6b0760fe1221d3ccc5203d7b0255a7c5d80633
Ivo Petrov <1414vo@gmail.com>
2024-06-13 13:21:54 +0000

    priors for dynesty

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ from sklearn.gaussian_process.kernels import (
 import numpy as np
 from numpy.typing import ArrayLike
 import pickle
+from scipy.stats import norm, uniform, loguniform
 
 
 def substract_activity(
@@ -75,3 +76,64 @@ def substract_activity(
 
     with open(hyperparam_path, "wb") as f:
         pickle.dump(hyperparams, f)
+
+
+class Prior:
+    """! A class for modelling prior distributions and sampling from them.
+
+    @param type     The type of prior. Currently supports the "uniform", "log-uniform" (Jeffrey's),
+    and "gaussian" priors.
+    @param bounds   Any relevant parameters for the distribution. Includes bounds for uniform distribution
+    or mean and std for the Gaussian"""
+
+    def __init__(self, type, bounds):
+        """! Creates a Prior object for a parameter.
+
+        @param type     The type of prior. Currently supports the "uniform", "log-uniform" (Jeffrey's),
+        and "gaussian" priors.
+        @param bounds   Any relevant parameters for the distribution. Includes bounds for uniform distribution
+        or mean and std for the Gaussian"""
+
+        assert type in [
+            "uniform",
+            "log-uniform",
+            "gaussian",
+        ], 'Possible Prior types include "uniform", "log-uniform" or "gaussian"'
+        self.bounds = bounds
+        self.type = type
+
+    def nll(self, X: ArrayLike) -> float:
+        """! Computes the negative log probability for a set of points.
+
+        @param X    The input data.
+        @returns    The negative log probability"""
+
+        if not hasattr(X, "__iter__"):
+            X = np.array([X])
+
+        if self.type == "uniform":
+            return -np.sum(
+                uniform.logpdf(
+                    X, loc=self.bounds[0], scale=self.bounds[1] - self.bounds[0]
+                )
+            )
+        elif self.type == "log-uniform":
+            return -np.sum(loguniform.logpdf(X, self.bounds[0], self.bounds[1]))
+        elif self.type == "gaussian":
+            return -np.sum(norm.logpdf(X, loc=self.bounds[0], scale=self.bounds[1]))
+        return None
+
+    def __call__(self, u: float) -> float:
+        """! Samples from the prior using the PPF of the distribution.
+
+        @param u    A number in the [0, 1] region.
+        @returns    The sampled parameter."""
+        if self.type == "uniform":
+            return uniform.ppf(
+                u, loc=self.bounds[0], scale=self.bounds[1] - self.bounds[0]
+            )
+        elif self.type == "log-uniform":
+            return loguniform.ppf(u, self.bounds[0], self.bounds[1])
+        elif self.type == "gaussian":
+            return norm.ppf(u, loc=self.bounds[0], scale=self.bounds[1])
+        return None
